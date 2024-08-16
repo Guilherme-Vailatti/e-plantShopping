6909680048355128bcdb62da6fb31d2525203009
Guilherme-Vailatti <guilherme.vailatti@hotmail.com>
2024-08-16 16:46:17 -0400

    Commite das ateracoes

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,14 +7,27 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
+         const { name, image, cost } = action.payload;
+  const existingItem = state.items.find(item => item.name === name); // Verifica se o item já existe no carrinho
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    state.items.push({ name, image, cost, quantity: 1 }); 
+  }
     
     },
     removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
-
-    
-    },
+        state.items = state.items.filter(item => item.name !== action.payload); // Remove o item cujo nome corresponde ao payload
+      },
+      
+      updateQuantity: (state, action) => {
+        const { name, quantity } = action.payload; // Extrai o nome e a nova quantidade do item
+        const itemToUpdate = state.items.find(item => item.name === name); // Encontra o item no carrinho
+        if (itemToUpdate) {
+          itemToUpdate.quantity = quantity; // Atualiza a quantidade do item
+        }
+      },
+      
   },
 });
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { Provider } from 'react-redux'; // Importa o Provider do react-redux
+import store from './store.js'; // Importa a store do arquivo store.js
+
+// Renderiza o aplicativo dentro do Provider para que a store seja acessível em toda a aplicação
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
-)
+);
